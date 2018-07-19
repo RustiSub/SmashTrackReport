49e8444567eb8881f95deb05fe3a2a2ca1c4e9e7
Wouter Pirotte <wouter.pirotte@saga.be>
2018-07-19 13:51:17 +0200

    More Leaderboard POCing

--- vueapp01/src/main.js
+++ vueapp01/src/main.js
@@ -28,27 +28,49 @@ let calculatePlayerStats = function(matches, users) {
     user.stocksLostRatio = 0;
 
     user.matchesPlayed = [];
+    user.characters = [];
 
     Array.from(matches).forEach(function(matchData) {
       Array.from(matchData.players).forEach(function(player) {
         if (player.user.id === user.id) {
           user.matchesPlayed.push(matchData);
+
+          if(!user.characters.hasOwnProperty(player.character.name)){
+            user.characters[player.character.name] = player.character;
+          }
+
+          let character = user.characters[player.character.name];
+
+          character.games = 0;
+          character.wins = 0;
+          character.losses = 0;
+          character.winRatio = 0;
+          character.stocksTaken = 0;
+          character.stocksTakenRatio = 0;
+          character.stocksLost = 0;
+          character.stocksLostRatio = 0;
         }
       });
     });
 
     Array.from(user.matchesPlayed).forEach(function(matchPlayedData) {
       Array.from(matchPlayedData.players).forEach(function(player) {
+        let character = user.characters[player.character.name];
+
         if (player.user.id === user.id) {
           if (player.is_winner) {
             user.wins += 1;
+            character.wins += 1;
           } else {
             user.losses += 1;
+            character.losses += 1;
           }
 
           user.games += 1;
+          character.games += 1;
 
           user.stocksLost += matchPlayedData.match.stocks - player.data.stocks;
+          character.stocksLost += matchPlayedData.match.stocks - player.data.stocks;
         } else {
           user.stocksTaken += matchPlayedData.match.stocks - player.data.stocks;
         }
