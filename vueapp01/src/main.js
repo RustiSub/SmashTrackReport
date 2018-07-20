0ee9df075a67cce7230edfcfb67772342eaea5f2
Wouter Pirotte <wouter.pirotte@saga.be>
2018-07-20 18:05:02 +0200

    Fix % in header and add Stage to Matches.

--- vueapp01/src/main.js
+++ vueapp01/src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false;
 
 Vue.filter('formatDate', function(value) {
   if (value) {
-    return moment(String(value)).format('MM/DD/YYYY hh:mm')
+    return moment(String(value)).format('MM/DD/YYYY')
   }
 });
 
